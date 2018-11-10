2a5ec8c5373530552f4078b1f5294f2b7fbee71c
adamdubicki <adam@freshworks.io>
2018-11-09 21:14:24 -0800

    new theming

--- src/components/HomeHeroBody.js
+++ src/components/HomeHeroBody.js
@@ -1,16 +1,38 @@
 import React from 'react';
 
+import Icon from './Icon';
+
 import profile from '../../assets/adam-icon.jpg';
 
 export default () => 
   <div className="home-hero-body hero-body">
     <div className="container has-text-centered typing">
-      <p className="home-hero-body__title is-1 title">
-        Hello, I'm Adam.
-      </p>
-      <img className="home-hero-body__profile" src={profile} alt="Adam Dubicki"/>
-      <p className="home-hero-body__subtitle is-1 subtitle">
-        I like to code.
-      </p>
+      <div className="columns">
+        <div className="column">
+          <img className="home-hero-body__profile" src={profile} alt="Adam Dubicki"/>
+          <p className="home-hero-body__title is-1 title">
+            Hello, I'm Adam.
+          </p>
+          <p className="home-hero-body__subtitle is-1 subtitle">
+            I like to code.
+          </p>
+        </div>
+        <div className="column home-hero-body__right-column">
+          <div className="columns">
+            <div className="column">
+              <Icon icon="fa fa-3x fa-server"/>
+              <p className="subtitle">Backend</p>
+            </div>
+            <div className="column">
+                <Icon icon="fa fa-3x fa-code"/>
+                <p className="subtitle">Frontend</p>
+            </div>
+            <div className="column">
+              <Icon icon="fa fa-3x fa-cloud-upload-alt"/>
+              <p className="subtitle">DevOps</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -6,7 +6,7 @@ import "../styles/main.scss"
 export default ({data, children}) => (
   <div id="app">
     <Helmet>
-      <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous"/>
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous"/>
     </Helmet>
     {children()}
   </div>
