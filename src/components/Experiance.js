ed52954fd14060ffabaf94bc5737e44736bd87ea
adamdubicki <adam@freshworks.io>
2019-01-26 11:36:43 -0800

    filled out experiance cards

--- src/components/Experiance.js
+++ src/components/Experiance.js
@@ -2,9 +2,97 @@ import React, { Component } from 'react';
 
 class Experiance extends Component {
   render() {
+    const eventCards = [
+      {
+        title: "Junior Full Stack Developer",
+        company: "Freshworks Studio",
+        time: "Oct 2017 - Present",
+        images: [
+          "https://media.licdn.com/dms/image/C560BAQHoZWFCL2LhEQ/company-logo_400_400/0?e=1556755200&v=beta&t=g0b0v4fxBrdqIAlywmC5-M2xzvBIHizxk8bp96gOnvQ"
+        ],
+        description: [
+          "Developed backend APIs using Laravel/Lumen & Express",
+          "Deployed releases on AWS Elastic Beanstalk with Docker",
+          "Created frontend web applications with React, Redux and Next.js",
+          "Integrated third-party API's such as Facebook Oauth, Stripe, Pusher and Google Vision"
+        ]
+      },
+      {
+        title: "Graduation",
+        company: "University of Victoria",
+        time: "Sep 2013 - Aug 2017",
+        images: [
+          "https://media.licdn.com/dms/image/C560BAQEo4ZvWzpKgEw/company-logo_400_400/0?e=1556755200&v=beta&t=NJgPPfcY_7qHmq3t1GQdA7h0ekUQ6TuS3j1Oy6s9CSM"
+        ],
+        description: [
+          "Bachelors Degree with distinction",
+          "Computer Science with Software Engineering Option",
+          "GPA: 7.13 / 9.0 "
+        ]
+      },
+      {
+        title: "Junior Back-end Developer",
+        company: "Giftbit + Lightrail",
+        time: "May 2016 - Dec 2016",
+        images: [
+          "https://media.licdn.com/dms/image/C510BAQGquDZ9MxMkzw/company-logo_400_400/0?e=1556755200&v=beta&t=MbZPOsm7BYQnMVtYtpfXM0PSZ5n2fd__eqiHxyRaYKw",
+          "https://media.licdn.com/dms/image/C4D0BAQHmQdJZDrQnQA/company-logo_400_400/0?e=1556755200&v=beta&t=D3JGiv5hO9YIc_AEmj0ZjbO8MkOcWO3pHz6JH1Pdl0g"
+        ],
+        description: [
+          "Developed backend APIs using Laravel/Lumen & Express",
+          "Deployed releases on AWS Elastic Beanstalk with Docker",
+          "Created frontend web applications with React, Redux and Next.js",
+          "Integrated third-party API's such as Facebook Oauth, Stripe, Pusher and Google Vision"
+        ]
+      },
+      {
+        title: "QA Analyst",
+        company: "IBM",
+        time: "Jan 2015 - Apr 2015",
+        images: [
+          "https://media.licdn.com/dms/image/C560BAQEZL5_LD7kuhg/company-logo_400_400/0?e=1556755200&v=beta&t=2F5prnODgL7RQJB_4BoqQ5XqxGLpfOrm6kdpA3HOqTM"
+        ],
+        description: [
+          "Developed backend APIs using Laravel/Lumen & Express",
+          "Deployed releases on AWS Elastic Beanstalk with Docker",
+          "Created frontend web applications with React, Redux and Next.js",
+          "Integrated third-party API's such as Facebook Oauth, Stripe, Pusher and Google Vision"
+        ]
+      }
+    ]
     return (
       <section className="experiance">
-        <div className="experiance__card">
+        {eventCards.map(({ title, company, time, images, description }) => (
+          <div className="experiance__card">
+            <div className="experiance__event">
+              <div className="experiance__image-wrapper">
+                {images.map(image => 
+                  <img 
+                    className="experiance__event-image"
+                    src={image}
+                  />
+                )}
+              </div>
+              <div className="experiance__event-company">
+                  { company }
+              </div>
+              <div className="experiance__event-title">
+                { title }
+              </div>
+              <div className="experiance__event-subtitle">
+                { time }
+              </div>
+            </div>
+            <div className="experiance__description">
+              <ul>
+                {description.map((item) => 
+                  <li>{item}</li>
+                )}
+              </ul>
+            </div>
+          </div>
+        ))}
+        {/* <div className="experiance__card">
           <div className="experiance__event">
             <div className="experiance__event-title">
               Freshworks Junior Full Stack Developer
@@ -18,11 +106,10 @@ class Experiance extends Component {
               <li>Developed backend APIs using Laravel/Lumen & Express</li>
               <li>Deployed releases on AWS Elastic Beanstalk with Docker</li>
               <li>Created frontend web applications with React, Redux and Next.js</li>
-              <li>Integrated third-party API's such as Facebook Oauth, Stripe, Pusher and Google Vision</li>
             </ul>
           </div>
-        </div>
-        <div className="experiance__card">
+        </div> */}
+        {/* <div className="experiance__card">
           <div className="experiance__event">
             <div className="experiance__event-title">
               Graduated from UVic
@@ -71,7 +158,7 @@ class Experiance extends Component {
               <li>Performed several database migrations and back-end deployments</li>              
             </ul>
           </div>
-        </div>
+        </div> */}
       </section>
     )
   }
