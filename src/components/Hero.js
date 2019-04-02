57df2db9cc237281bd5fd7c984108547b097318b
adamdubicki <adam@freshworks.io>
2019-04-01 17:46:01 -0700

    refactoring

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,27 +1,11 @@
 import React, { Component } from 'react';
-
-import Icon from './Icon';
-import { FRONT_END, BACK_END, DEV_OPS } from '../constants/Icons';
-import profile from '../assets/adam-icon.jpg';
 import Fade from 'react-reveal/Fade';
+
+import Icon from '../components/Icon';
 
 class Hero extends Component {
   state = {
     subtitleIndex: 0,
-    subtitles: [
-      {
-        text: 'Front-End',
-        icon: FRONT_END
-      },
-      {
-        text: 'Back-End',
-        icon: BACK_END
-      },
-      {
-        text: 'DevOps',
-        icon: DEV_OPS
-      }
-    ],
     show: false
   }
 
@@ -30,7 +14,7 @@ class Hero extends Component {
       const { show, subtitleIndex, subtitles } = this.state;
       this.setState({ 
         show: !show,
-        subtitleIndex: (subtitleIndex + 1) % subtitles.length 
+        subtitleIndex: (subtitleIndex + 1) % 10
       });
     }, 1500);
   }
@@ -40,27 +24,45 @@ class Hero extends Component {
   }
 
   render() {
-    const { subtitleIndex, subtitles } = this.state;
-    const { text, icon } = subtitles[subtitleIndex];
     return (
       <section className="hero">
-        <div className="hero__profile-wrapper">
-          <img className="hero__profile" src={profile}/>
-        </div>
-        <header className="hero__header">
-          <h1>
-            Hello, my name is <strong>Adam</strong>
-          </h1>
-          <h2>I am a <strong>Full Stack Software Developer</strong></h2>
-          <Fade bottom opposite when={this.state.show}>
-            <h2>
-              <strong>{text}</strong>&nbsp;<Icon icon={icon}/>
-            </h2>
-          </Fade>
-        </header>
+        {this.props.children}
       </section>
     )
   }
 }
 
-  export default Hero;
+const Profile = ({ profile }) => (
+  <div className="hero__profile-wrapper">
+    <img className="hero__profile" src={profile}/>
+  </div>
+);
+
+const Header = ({ children }) => (
+  <header className="hero__header">
+    { children }
+  </header>
+)
+
+const Scroller = ({children }) => {
+  return (
+    // <Fade bottom opposite when={this.state.show}>
+    //   {children}
+    // </Fade>
+    <div>{children[0]}</div>
+  )
+}
+
+const ScrollItem = ({ text, icon }) => (
+  <h2>
+    <strong>{text}</strong>&nbsp;
+    <Icon icon={icon}/>
+  </h2>
+)
+
+Hero.Profile = Profile;
+Hero.Header = Header;
+Hero.Scroller = Scroller;
+Hero.ScrollItem = ScrollItem;
+
+export default Hero;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 
+import profile from '../assets/adam-icon.jpg';
+import { FRONT_END, BACK_END, DEV_OPS } from '../constants/Icons';
+
 import Hero from '../components/Hero';
 import AboutMe from '../components/AboutMe';
 
@@ -7,7 +10,18 @@ class Index extends Component {
   render() {
     return (
       <main>
-        <Hero />
+        <Hero>
+          <Hero.Profile profile={profile}/>
+          <Hero.Header>
+            <h1>Hello, my name is <strong>Adam</strong></h1>
+            <h2>I am a <strong>Full Stack Software Developer</strong></h2>
+          </Hero.Header>
+          <Hero.Scroller>
+            <Hero.ScrollItem text='Front-End' icon={FRONT_END}/>
+            <Hero.ScrollItem text='Back-End'icon={BACK_END}/>
+            <Hero.ScrollItem text='DevOps' icon={DEV_OPS}/>
+          </Hero.Scroller>
+        </Hero>
         {/* <Skills /> */}
         <AboutMe />
         {/* <Experiance /> */}
