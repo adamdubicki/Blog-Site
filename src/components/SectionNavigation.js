a012712006124d8c848371521247bb4d976bfaa0
adamdubicki <adam@freshworks.io>
2019-04-18 12:16:54 -0700

    more work on the navbar

--- src/components/SectionNavigation.js
+++ src/components/SectionNavigation.js
@@ -8,7 +8,24 @@ import Icon from './Icon';
 class SectionNavigation extends Component {
 
   state={
-    isOpen: false
+    isOpen: false,
+    currentHash: ''
+  }
+
+  componentDidMount() {
+    this.handleHashChange();
+    window.addEventListener('hashchange', this.handleHashChange, false);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('hashchange', this.handleHashChange, false);
+  }
+
+  handleHashChange = () => {
+    const { hash } = window.location;
+    this.setState({ 
+      currentHash: hash
+    });
   }
 
   onBurgerClick = () => {
@@ -18,7 +35,19 @@ class SectionNavigation extends Component {
   }
 
   render() {
-    const { isOpen } = this.state;
+    const { isOpen, currentHash } = this.state;
+    const isFloating = (currentHash !== '#home' && currentHash !== '');
+
+    const sectionClassNames = classNames(
+      'section-navigation',
+      { 'is-floating': isFloating }
+    );
+
+    const sectionLinkClassNames = classNames(
+      'section-navigation__links',
+      { 'is-open': isOpen },
+      { 'is-floating': isFloating }
+    );
 
     const burgerClassNames = classNames(
       'section-navigation__hamburger',
@@ -26,17 +55,15 @@ class SectionNavigation extends Component {
       { 'is-open': isOpen }
     );
 
-    const sectionLinkClassNames = classNames(
-      'section-navigation__links',
-      { 'is-open': isOpen }
-    );
-
-    const linkClassNames = classNames(
-      { 'is-bordered': !isOpen }
-    );
+    const links = [
+      'About Me',
+      'Skills',
+      'Experiance',
+      'Contact'
+    ];
 
     return (
-      <nav className="section-navigation">
+      <nav className={sectionClassNames}>
         <button
           className={burgerClassNames}
           onClick={() => this.onBurgerClick()}
@@ -44,10 +71,22 @@ class SectionNavigation extends Component {
           <Icon icon={isOpen ? TIMES : BARS}/>
         </button>
         <div className={sectionLinkClassNames}>
-          <a className={linkClassNames} href="#about-me">About Me</a>
-          <a className={linkClassNames} href="#skills">Skills</a>
-          <a className={linkClassNames} href="#experiance">Experiance</a>
-          <a className={linkClassNames} href="#contact">Contact</a>
+          {links.map((link) => {
+            const href = `#${link.split(' ').join('-').toLowerCase()}`;
+            const linkClassNames = classNames(
+              { 'is-active': href === currentHash },
+              { 'is-bordered': !isOpen },
+            );
+            return (
+              <a 
+                className={linkClassNames} 
+                href={href}
+                key={link}
+              >
+                {link}
+              </a>
+            )
+          })}
         </div>
       </nav>
     )
--- src/components/section/Hero.js
+++ src/components/section/Hero.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { FRONT_END, BACK_END, DEV_OPS } from '../../constants/Icons';
 import profile from '../../assets/adam-icon.jpg';
 
+import ScrollableAnchor from 'react-scrollable-anchor'
 import Scroller from '../Scroller';
 import Icon from '../Icon';
 import SectionNavigation from '../SectionNavigation';
@@ -10,21 +11,23 @@ import SectionNavigation from '../SectionNavigation';
 class Hero extends Component {
   render() {
     return (
-      <section className="hero">
-        <div className="hero__profile-wrapper">
-          <img className="hero__profile" src={profile}/>
-        </div>
-        <div className="hero__header">
-          <h1 className="title">Hello, my name is <strong>Adam</strong></h1>
-          <h2 className="title">I am a <strong>Full Stack Software Developer</strong></h2>
-        </div>
-        {/* <Scroller className="hero__scroller">
-          <p><strong>Front End</strong>&nbsp;<Icon icon={FRONT_END}/></p>
-          <p><strong>Back End</strong>&nbsp;<Icon icon={BACK_END}/></p>
-          <p><strong>DevOps</strong>&nbsp;<Icon icon={DEV_OPS}/></p>
-        </Scroller> */}
-        <SectionNavigation />
-      </section>
+      <ScrollableAnchor id={'home'}>
+        <section className="hero">
+          <div className="hero__profile-wrapper">
+            <img className="hero__profile" src={profile}/>
+          </div>
+          <div className="hero__header">
+            <h1 className="title">Hello, my name is <strong>Adam</strong></h1>
+            <h2 className="title">I am a <strong>Full Stack Software Developer</strong></h2>
+          </div>
+          <Scroller className="hero__scroller">
+            <p><strong>Front End</strong>&nbsp;<Icon icon={FRONT_END}/></p>
+            <p><strong>Back End</strong>&nbsp;<Icon icon={BACK_END}/></p>
+            <p><strong>DevOps</strong>&nbsp;<Icon icon={DEV_OPS}/></p>
+          </Scroller>
+          <SectionNavigation />
+        </section>
+      </ScrollableAnchor>
     )
   }
 }
